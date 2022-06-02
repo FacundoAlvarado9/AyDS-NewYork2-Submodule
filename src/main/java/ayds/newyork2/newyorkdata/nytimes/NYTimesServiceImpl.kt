7f0035670c1fc7d7a17ffeb059e1882a92060a06
Genaro08 <71444929+Genaro08@users.noreply.github.com>
2022-06-02 20:13:31 -0300

    Visibilidad internal en NYTimesServiceImpl

--- src/main/java/ayds/newyork2/newyorkdata/nytimes/NYTimesServiceImpl.kt
+++ src/main/java/ayds/newyork2/newyorkdata/nytimes/NYTimesServiceImpl.kt
@@ -2,7 +2,7 @@ package ayds.newyork2.newyorkdata.nytimes
 
 import retrofit2.Response
 
-class NYTimesServiceImpl (private val artistResolver : NYTimesToArtistResolver, private val apiFromNYTimes : NYTimesAPI):
+internal class NYTimesServiceImpl (private val artistResolver : NYTimesToArtistResolver, private val apiFromNYTimes : NYTimesAPI):
     NYTimesService {
 
     override fun getArtist(artistName: String) : NYTimesArtistInfo? {
